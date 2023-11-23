df2c00c104c2d0338dd0ea164b3501ce42634621
sxeix <sxeixarts@gmail.com>
2023-11-23 23:00:35 +0000

    Use enums instead of strings and also general refactoring

--- src/jwt.rs
+++ src/jwt.rs
@@ -1,3 +1,4 @@
+use crate::project_info;
 use jsonwebtoken::{encode, EncodingKey, Header};
 use serde::{Deserialize, Serialize};
 
@@ -11,7 +12,11 @@ struct Project2 {
     another: String,
 }
 
-pub fn generate_jwt(project_type: &String, content: &String, secret: &String) -> String {
+pub fn generate_jwt(
+    project_type: &project_info::Project,
+    content: &String,
+    secret: &String,
+) -> String {
     let encoding_key_result = EncodingKey::from_base64_secret(secret);
 
     let encoding_key = match encoding_key_result {
@@ -22,8 +27,8 @@ pub fn generate_jwt(project_type: &String, content: &String, secret: &String) ->
         }
     };
 
-    match project_type.as_str() {
-        "project1" => {
+    match project_type {
+        project_info::Project::Project1 => {
             let my_claims_result = serde_json::from_str::<Project1>(content);
             let my_claims = match my_claims_result {
                 Ok(result) => result,
@@ -41,7 +46,7 @@ pub fn generate_jwt(project_type: &String, content: &String, secret: &String) ->
                 }
             };
         }
-        "project2" => {
+        project_info::Project::Project2 => {
             let my_claims_result = serde_json::from_str::<Project2>(content);
             let my_claims = match my_claims_result {
                 Ok(result) => result,
